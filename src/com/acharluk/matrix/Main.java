ec128411e19eda467c826a2dc02191f57c2a92c2
ACharLuk <acharluk@gmail.com>
2014-09-21 16:58:46 +0200

    Print method completed

--- src/com/acharluk/matrix/Main.java
+++ src/com/acharluk/matrix/Main.java
@@ -1,5 +1,7 @@
 package com.acharluk.matrix;
 
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -7,16 +9,29 @@ public class Main {
     public static void main(String[] args) {
     }
 
+    //Multiply matrix a with matrix b
+    //Returns an int[][] result
     public static int[][] multiply(int[][] a, int[][] b) {
-        
+        int result[][] = new int[a.length][b.length];
+        //Calcs here
+        return result;
     }
 
+    //Print matrix with format
     public static void print(int[][] a) {
-
+        System.out.println();
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
     }
 
+    //Read matrix from user input and save it
     public static int[][] readMatrix() {
-
+        Scanner sc = new Scanner(System.in);
+        //Transform input String to int[][]
     }
 
 }
