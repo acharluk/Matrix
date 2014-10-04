ee9849d1e2aa9d57885dfaf220e02fa83354ead8
ACharLuk <acharluk@gmail.com>
2014-10-04 17:25:58 +0200

    More code cleaning

--- src/com/acharluk/matrix/MatrixGUI.java
+++ src/com/acharluk/matrix/MatrixGUI.java
@@ -32,61 +32,13 @@ public class MatrixGUI extends JFrame {
         calculateButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                /*
                 ma = textArea1.getText();
                 mb = textArea3.getText();
                 System.out.println(textArea1.getText());
                 System.out.println(textArea3.getText());
-                print(Main.multiply(readMatrixFromGUI(ma), readMatrixFromGUI(mb)));
+                */
             }
         });
     }
-
-    public int[][] readMatrixFromGUI(String m) {
-
-        int sizeX = (int) Math.sqrt(m.length());
-        int sizeY = sizeX;
-
-        //Create matrix
-        int[][] matrix = new int[sizeX][sizeY];
-
-        String[] nums = m.split("");
-        int[] numsInt = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            int k = 0;
-            if (nums[k] == "" || nums[k] == " " || nums[k] == "\n") {
-                nums[k] = nums[k].substring(1);
-            } else {
-                try {
-                    numsInt[i] = Integer.parseInt(nums[k]);
-                } catch (NumberFormatException e) {
-                    e.printStackTrace();
-                }
-                k++;
-            }
-        }
-        int k = 0;
-        for (int i = 0; i < sizeX; i++) {
-            for (int j = 0; j < sizeY; j++) {
-                    matrix[i][j] = numsInt[k];
-                    k++;
-            }
-        }
-        return matrix;
-    }
-
-    public void print(int[][] a) {
-        String res = "";
-        System.out.println();
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                System.out.print(a[i][j] + " ");
-                res = res.concat(a[i][j] + " ");
-            }
-            System.out.println();
-            res = res.concat("\n");
-        }
-        System.out.println("RES: " + res);
-        textArea2.setText(res);
-    }
-
 }
