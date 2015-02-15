5ee810f1066611c823de6570442b6485b5c7162d
ACharLuk <acharluk@gmail.com>
2015-02-15 09:23:06 +0100

    Fixed multiply method

--- src/com/acharluk/matrix/Matrix.java
+++ src/com/acharluk/matrix/Matrix.java
@@ -23,6 +23,15 @@ public class Matrix {
         }
     }
 
+    public Matrix(int[][] mat, int mSize) {
+        size = mSize * mSize;
+        matrix = mat;
+    }
+
+    public Matrix(int mSize) {
+        size = mSize * mSize;
+    }
+
     public void setSize(int size) {
         this.size = size;
     }
@@ -38,10 +47,12 @@ public class Matrix {
         System.out.print("Matrix: ");
         String s = scanner.next();
         String[] ss = s.split(" ");
-        int[] mat = new int[size];
+        int[][] mat = new int[size][size];
 
         for (int i = 0; i < s.length(); i++) {
-            mat[i] = Integer.parseInt(ss[i]);
+            for (int j = 0; j < s.length(); j++) {
+                mat[i][j] = Integer.parseInt(ss[i]);
+            }
         }
         Matrix m = new Matrix(mat, size);
 
@@ -49,13 +60,16 @@ public class Matrix {
     }
 
     public Matrix multiply(Matrix mat) {
-        Matrix result = new Matrix(new int[size], size);
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                int res = 0;
+        Matrix result = new Matrix(mat.matrix.length);
+        for (int i = 0; i < this.matrix.length; i++)
+            for (int j = 0; j < mat.matrix[i].length; j++)
+                result.matrix[i][j] = 0;
 
-                for (int k = 0; k < matrix[i].length; k++) {
-                    res += matrix[i][k] * mat.matrix[k][j];
+        for (int i = 0; i < result.matrix.length; i++) {
+            for (int j = 0; j < result.matrix[i].length; j++) {
+                int res = 0;
+                for (int k = 0; k < this.matrix[i].length; k++) {
+                    res += this.matrix[i][k] * mat.matrix[k][j];
                 }
                 result.matrix[i][j] = res;
                 System.out.println("result[" + i + "][" + j + "] = " + result.matrix[i][j]);
