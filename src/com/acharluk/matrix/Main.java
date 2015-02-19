5d090bc82f87f0e3bd8a99da7010109533e14208
ACharLuk <acharluk@gmail.com>
2015-02-19 18:30:45 +0100

    Working GUI

--- src/com/acharluk/matrix/Main.java
+++ src/com/acharluk/matrix/Main.java
@@ -1,16 +1,11 @@
 package com.acharluk.matrix;
 
-import java.util.Scanner;
-
 public class Main {
 
-    public static boolean DEBUG = true;
-
     public static void main(String[] args) {
-        //new MatrixGUI();
-        //print(multiply(readMatrix(),readMatrix()));
+        new MatrixGUI();
 
-        new Main();
+        //new Main();
     }
 
     public Main() {
@@ -18,59 +13,6 @@ public class Main {
         Matrix m2 = Matrix.read();
         Matrix res = m1.multiply(m2);
         res.print();
-
-    }
-
-    public static int[][] multiply(int[][] a, int[][] b) {
-        int result[][] = new int[a.length][b.length];
-        for (int i = 0; i < a.length; i++)
-            for (int j = 0; j < b[i].length; j++)
-                result[i][j] = 0;
-
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[i].length; j++) {
-                int res = 0;
-                for (int k = 0; k < a[i].length; k++) {
-                    res += a[i][k] * b[k][j];
-                }
-                result[i][j] = res;
-                System.out.println("result[" + i + "][" + j + "] = " + result[i][j]);
-            }
-        }
-        return result;
-    }
-
-    public static void print(int[][] a) {
-        System.out.println();
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[0].length; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    //Read matrix from user input and save it
-    public static int[][] readMatrix() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter matrix: ");
-
-        //Transform input String to int[][]
-        String str = sc.nextLine();
-        String[] nums = str.split(" ");
-
-        int size = (int) Math.sqrt(nums.length);
-        int[][] matrix = new int[size][size];
-
-        int k = 0;
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = Integer.parseInt(nums[k]);
-                if (DEBUG) System.out.println("matrix[" + i + "][" + j + "] = " + nums[k]);
-                k++;
-            }
-        }
-        return matrix;
     }
 
 }
